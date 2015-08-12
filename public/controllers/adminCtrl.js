ec302f441a48f1483b94cddab3ec0b529aaa1419
EricScottRichards <ericscottrichards@gmail.com>
2015-08-12 10:26:11 -0600

    more done

--- public/controllers/adminCtrl.js
+++ public/controllers/adminCtrl.js
@@ -1,3 +1,30 @@
 app.controller('adminCtrl', function($scope, mainSrvc){
-	$scope.test = "yerpinstein"
+	// $scope.test = "yerpinstein"
+	// var getStuff = function(){
+	// 	mainSrvc.getStuff().then(function(response){
+	// 		console.log(response)
+	// 		$scope.allStuff = response
+	// 	})
+	// }
+
+	// getStuff();
+
+	$scope.allStuff = mainSrvc.getStuff();
+
+	$scope.postData = function(){
+		console.log($scope.item);
+		mainSrvc.postData($scope.item)
+	}
+
+	$scope.deleteData = function(){
+		mainSrvc.deleteData()
+	}
+
+	$scope.updateData = function(){
+		
+	}
+
+	// $scope.postData = function(){
+		
+	// }
 })
--- public/services/mainSrvc.js
+++ public/services/mainSrvc.js
@@ -1,11 +1,27 @@
 app.service('mainSrvc', function($q, $http){
-	var dfd = $q.defer();
+	
+
+	
 	this.getStuff = function(){
+		var dfd = $q.defer();
 		$http.get('/api/eCommerceThing')
 			.then(function(response){
 				console.log(response);
 				dfd.resolve(response.data);
 			})
 		return dfd.promise;
+	}
+
+	this.postData = function(item){
+		console.log(item);
+		return $http({
+			method: "POST",
+			url: "/api/eCommerceThing",
+			data: {
+				name: item.name,
+				description: item.desc,
+				price: item.price
+			}
+		})
 	}
 })
